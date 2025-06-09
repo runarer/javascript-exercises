1a90be36d790ffe838d080f4a3bb4350e25bda58
runarer <95697611+runarer@users.noreply.github.com>
2025-06-09 13:47:54 +0200

    Solves part 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeAll) {
+    removeAll.forEach( (remove) => {
+        while(arr.includes(remove)) {
+            const indexOfRemove = arr.findIndex(item => item === remove);
+            arr.splice(indexOfRemove,1);
+        }
+    });
+    return arr;
 };
+
+/* Denne fra løsningen er bedre*/
+// const removeFromArray = function(array, ...args) {
+//     return array.filter(val => !args.includes(val))
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
