a4c35de3b19eac2e463fd19f86be3dec51df7f82
runarer <95697611+runarer@users.noreply.github.com>
2025-06-09 13:29:27 +0200

    Solves part 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+    let repeatedString = (times < 0) ? "ERROR" : "";
+    for (let repeated = 0; repeated < times; repeated++) {
+        repeatedString += str
+    }
+    return repeatedString;    
 };
 
 // Do not edit below this line
