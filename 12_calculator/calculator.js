a16966062388c9f53bd7407bc157695ec2cb2c11
runarer <95697611+runarer@users.noreply.github.com>
2025-06-13 08:58:26 +0200

    Solves part 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc,number) => acc + number, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if (numbers.length < 1) return 0;
+  return numbers.reduce( (acc,number) => acc*number,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(size) {
+	let answer = 1
+  for (let i = 1; i <= size; i++) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
