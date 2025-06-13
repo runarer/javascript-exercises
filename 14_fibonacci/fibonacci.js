2dbb2769860624f1751b3852490a1620f213e907
runarer <95697611+runarer@users.noreply.github.com>
2025-06-13 09:35:18 +0200

    Solves part 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNumber) {
+    const sequenceNr = parseInt(inputNumber);
+        
+    if (sequenceNr < 0) return "OOPS";
+    
+    let sequence = [0,1,1]
+    while (sequence.length <= sequenceNr) {        
+        sequence.push(sequence.at(-1) + sequence.at(-2) )
+    }
+    
+    console.log(sequence)
+    return sequence[inputNumber];
 };
 
 // Do not edit below this line
