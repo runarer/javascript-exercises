82731a24e521aa097c342f5afd40b20ea7ce005d
runarer <95697611+runarer@users.noreply.github.com>
2025-06-13 09:22:23 +0200

    Solves part 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/\W/g,'');
 
+    // Can do a split, reverse, join and compare strings.
+    
+    for (let start = 0, end = newStr.length - 1; start < end; start++, end--) {
+        if(newStr[start] !== newStr[end]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
